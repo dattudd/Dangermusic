50873ef4c84c6a5bd39d39984aa912ca4d72987c
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-01-08 16:37:50 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,7 +26,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["𝐎𝐰𝐧𝐞𝐫"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["𝐆𝐫𝐨𝐮𝐩"], url=config.SUPPORT_CHAT),
         ],
         [
