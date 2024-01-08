4c0a37de761ca8e85117316c532b89e8a18dc0a7
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-01-08 16:52:29 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["𝐆𝐫𝐨𝐮𝐩"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -27,10 +27,10 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["𝐆𝐫𝐨𝐮𝐩"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["𝐔𝐩𝐝𝐚𝐭𝐞𝐬📢"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
