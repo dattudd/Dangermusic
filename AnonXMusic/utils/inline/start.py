071658a351b469a403db3cfa03bbb6e5468eb211
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-01-08 14:50:14 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,8 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.myra_updates),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+        ]
         ],
     ]
     return buttons
