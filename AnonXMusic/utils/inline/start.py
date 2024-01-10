176fc80f764e90e8192d105d21299946860eddf5
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-01-10 17:35:16 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -30,7 +30,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_7"], url="https://telegra.ph/file/b1f8ff56996c3d952fa48.mp4"),
+    
         ],
     ]
     return buttons
